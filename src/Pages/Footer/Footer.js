6298104ded7a5bcd6b1781096c12b7dc8efe8eac
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-17 19:15:36 +0600

    footer section done

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer className="footer bg-dark text-white p-5 text-center">
         <p>Developed by Jubayer Ahmmod Shuvo.</p>
-        <hr className="w-50 mx-auto" />
+        <hr className="w-25 mx-auto" />
         <p>© 2022 Company, Inc. All rights reserved.</p>
       </footer>
     </div>
