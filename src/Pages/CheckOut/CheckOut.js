c8beda415f1f0f67e146671623a0b1bc279907fb
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-18 15:04:28 +0600

    checkout page confirmation

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const CheckOut = () => {
+  const [email,setEmail]=useState("");
+  const [name,setName]=useState("");
+  const [phone, setPhone] = useState("");
+  const navigate=useNavigate();
+
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleNameBlur = (e) => {
+    setName(e.target.value);
+  };
+  const handlePhoneBlur = (e) => {
+    setPhone(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name, email, phone) {
+       navigate("/thankyou");
+    }
+    else {
+      alert("Please fill all the fields");
+    }
+   
+  }
+
+
   return (
     <div>
-      <h1>This is adfjijfbout</h1>
+      <div className="container  my-5 border rounded w-50 ">
+        <form>
+          <div className="form-group my-3">
+            <input
+              onBlur={handleNameBlur}
+              type="text"
+              className="form-control my-3 "
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter Name"
+            />
+
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group my-3">
+            <input
+              onBlur={handlePhoneBlur}
+              type="text"
+              className="form-control "
+              id="exampleInputPassword1"
+              placeholder="Phone Number"
+            />
+          </div>
+
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-primary my-3 "
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
