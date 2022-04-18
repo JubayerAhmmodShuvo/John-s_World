4677c7ef04ba27d03166cbebc0acd5a48f2ebb88
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-18 15:10:55 +0600

    checkout done

--- src/Pages/GetUpdates/GetUpdates.js
+++ src/Pages/GetUpdates/GetUpdates.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import update from "../../../src/images/updates.jpg";
 
 const GetUpdates = () => {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (( email)) {
+      navigate("/thankyou");
+    } else {
+      alert("Please enter email");
+    }
+  };
   return (
     <div>
       <div className="row my-5">
@@ -12,8 +25,8 @@ const GetUpdates = () => {
         <div className="col-md-6 col-lg-6 col-lg-6 ">
           <h1 className="name text-center my-lg-5 mt-5">Get The Latest Updates  </h1>
           <div className="input-group mb-3 w-75 mx-auto my-5">
-  <input type="text" className="form-control " placeholder="Enter Email" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-  <Link to="/thankyou" className="btn btn-outline-secondary" type="button" id="button-addon2">Get Updates</Link>
+  <input onBlur={handleEmailBlur} type="text" className="form-control " placeholder="Enter Email" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+  <button onClick={handleSubmit}   className="btn btn-outline-secondary" type="button" id="button-addon2">Get Updates</button>
 </div>
         </div>
       </div>
